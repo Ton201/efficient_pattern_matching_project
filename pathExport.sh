1f16b4be810cfc7d8e9e6e3412a85431d8691717
Antonin Reh <antonin.reh@gmail.com>
2025-12-06 17:25:30 +0100

    Add SuffixAutomaton.build

---
+++ pathExport.sh
@@ -0,0 +1,1 @@
+export PYTHONPATH=$PYTHONPATH:/home/antonin/Documents/vscht/NI-EVY-project2025/python
--- python/SAM.py
+++ python/SAM.py
@@ -6,9 +6,9 @@ class State:
   
     def __init__(self):
         self.suffix_link = None
-        self.next = {}
-  
-  
+        self.next = {} # dictionary: keys = characters, values = states you transition to on that character
+        self.length = 0
+        self.initial = False 
     def goto(self, c):
         """Return destination state id on character `c`, or None if absent."""
         return self.next.get(c, None)
@@ -56,8 +56,51 @@ class SuffixAutomaton:
         Build the automaton from text T.
         """
         self._alphabet = set(T)
-        return self._alphabet
-        raise NotImplementedError("TODO: implement build()")
+        n = len(T)
+        # set the last pointer to the innitial state
+        # length 0 and sl = None set when initialiying the SAM
+        last = None
+        # itterate thought the text T and extend the SAM
+        for letter in T:
+            # create a new state
+            new = State()
+            # set the depth of the state
+            len = self.states[last].length + 1
+            # set pointer to the last added state
+            p = last
+            while not (p is None and letter not in self.states[p].next.keys)
+                self.states[p].next[letter] = new # new is not an index but a node!
+                p = self.states[p].suffix_link
+            # if p is initial state suffix link of the first node is an initial state
+            if p is None:
+                new.suffix_link = 0
+            else:
+                q = self.states[p].next[c]
+                # if q is the next node
+                if self.states[p].length + 1 == self.states[p].length:
+                    new.suffix_link = q # q is not an index but a node!
+                # else need a clone
+                else:
+                    clone = State()
+                    clone.length = q.length + 1
+                    # copy transitions from OG node q
+                    for b in self._alphabet:
+                        clone.next[b] = q.next[b]
+                    new.suffix_link = clone
+                    clone.suffix_link = q.suffix_link # clone inherits suffix links from OG node
+                    q.suffix_link = clone
+                    while (p is None and letter not in self.states[p].next.keys):
+                        self.states[p].next[letter] = clone # clone is not an index but a node!
+                        p = self.states[p].suffix_link
+            self.states.append(current)
+            last = new
+        # end of extension
+        p = last
+        while p is not None:
+            self.states[p].final = True
+            p = self.states[p].suffix_link
+        return self
+        # raise NotImplementedError("TODO: implement build()")
     
   
     def count(self, P) -> int:
